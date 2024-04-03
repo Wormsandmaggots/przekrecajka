b53f2ec06c16b2e5bbecea05cbb9eebd07a65ea9
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2024-04-03 14:18:35 +0200

    .

---
+++ Assets/Scripts/Giroscope.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Giroscope : MonoBehaviour
+{
+    void Awake()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.gyro.gravity);
+        Physics2D.gravity = Input.gyro.gravity;
+    }
+    
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+}
