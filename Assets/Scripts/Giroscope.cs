0cdd048b169556231221c15d154af7c682c359b5
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2024-04-30 13:16:19 +0200

    .

--- Assets/Scripts/Giroscope.cs
+++ Assets/Scripts/Giroscope.cs
@@ -13,8 +13,10 @@ public class Giroscope : MonoBehaviour
 
     void Update()
     {
-        Quaternion q = GyroToUnity(Input.gyro.attitude);
-        Vector2 g = new Vector2(q.y + gravityOffset.x, -q.x + gravityOffset.y);
+        // Quaternion q = GyroToUnity(Input.gyro.attitude);
+        // Vector2 g = new Vector2(q.y + gravityOffset.x, -q.x + gravityOffset.y);
+
+        Vector2 g = new Vector2(Input.gyro.gravity.x, Input.gyro.gravity.y);
         
         Physics2D.gravity = g * gravityPower * GravityMultiplier;
     }
