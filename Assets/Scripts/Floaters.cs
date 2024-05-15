7861f3fc64522f557a6b5989cbe718db544b5131
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2024-05-15 22:56:47 +0200

    .

--- Assets/Scripts/Floaters.cs
+++ Assets/Scripts/Floaters.cs
@@ -1,22 +1,28 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class Floaters : MonoBehaviour
 {
-    [SerializeField] private Vector2 speedLimit = new Vector2(5,5);
+    [SerializeField] private Vector2 speedLimitX = new Vector2(5,5);
+    [SerializeField] private Vector2 speedLimitY = new Vector2(5, 5);
+    private Animator animator;
     private SpriteRenderer spriteRenderer;
     private SpriteMask spriteMask;
     private Vector2 direction;
     private Vector2 speed;
-    
+
     private void Start()
     {
+        animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteMask = GetComponent<SpriteMask>();
         direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
         direction = direction.normalized;
-        speed = new Vector2(Random.Range(1, speedLimit.x), Random.Range(1, speedLimit.y));
+        speed = new Vector2(Random.Range(speedLimitX.x, speedLimitX.y), Random.Range(speedLimitY.x, speedLimitY.y));
+
+        StartCoroutine(Delay(Random.Range(0f, 2f)));
     }
 
     private void Update()
@@ -25,29 +31,39 @@ public class Floaters : MonoBehaviour
 
         Vector2 m = direction * speed * Time.deltaTime;
         
-        transform.position += new Vector3(m.x + Mathf.Sin(Time.deltaTime), m.y + Mathf.Sin(Time.deltaTime), 0);
+        transform.position += new Vector3(m.x, m.y, 0);
     }
-
+    
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("bubbleHolder"))
         {
-            Vector2 point = other.ClosestPoint(transform.position);
+            Vector2 position = transform.position;
+            Vector2 point = other.ClosestPoint(position);
             
-            RaycastHit2D xHit = Physics2D.Raycast(point, new Vector2(-direction.x, 0), 1f,
-                Settings.platforms);
-            RaycastHit2D yHit = Physics2D.Raycast(point, new Vector2(0, -direction.y), 1f,
-                Settings.platforms);
-
-            if (xHit)
+            RaycastHit2D xHit = Physics2D.Raycast(position, new Vector2(direction.x > 0 ? -1 : 1, 0), 1f,
+                Settings.bubbleHolder);
+            RaycastHit2D yHit = Physics2D.Raycast(position, new Vector2(0, direction.y > 0 ? -1 : 1), 1f,
+                Settings.bubbleHolder);
+            
+            if (xHit.collider != null)
             {
                 direction.x = -direction.x;
             }
-
-            if (yHit)
+            
+            if (yHit.collider != null)
             {
                 direction.y = -direction.y;
             }
         }
+    }
+
+    private IEnumerator Delay(float time)
+    {
+        animator.speed = 0;
+
+        yield return new WaitForSeconds(time);
+
+        animator.speed = 1;
     }
 }
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -7,12 +7,15 @@ public class Settings : MonoBehaviour
 {
     public static LayerMask platforms;
     public static Heart heart;
+    public static LayerMask bubbleHolder;
     [SerializeField] private LayerMask platformLayer;
+    [SerializeField] private LayerMask bubbleHolderLayer;
     [SerializeField] private Heart heartPrefab;
 
     private void Start()
     {
         heart = heartPrefab;
         platforms = platformLayer;
+        bubbleHolder = bubbleHolderLayer;
     }
 }
