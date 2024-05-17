99488960361668819a20822f970db0ecefc0b96f
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2024-05-17 17:33:11 +0200

    ,

--- Assets/Scripts/Input/Accelerometr.cs
+++ Assets/Scripts/Input/Accelerometr.cs
@@ -15,6 +15,7 @@ public class Accelerometr : MonoBehaviour
     [SerializeField] private Vector2 minimalAccelerationThreshold = new Vector2(1, 1);
     [SerializeField] private float activeateDelay = 0.5f;
     [SerializeField] private Vector2 gyroDelta = new Vector2(1f, 1.5f);
+    [SerializeField] private bool accelerometer = true;
     private float earthGravityValue = 9.807f;
     private Vector2 prevAcceleration;
     private Vector2 prevGyro;
@@ -38,64 +39,82 @@ public class Accelerometr : MonoBehaviour
         Vector3 gyro = Input.gyro.gravity;
         Vector3 gyroRate = Input.gyro.rotationRate;
         Vector2 acc = new Vector2(Input.acceleration.x, Input.acceleration.y);
-        
         Vector2 unchangedAcc = Vector2.zero;
 
-        if (gyro.y <= 0)
+        if (accelerometer)
         {
-            acc.y += g;
-            acc.y = -acc.y;
+            if (gyro.y <= 0)
+            {
+                acc.y += g;
+                acc.y = -acc.y;
+            }
+            else
+            {
+                acc.y -= g;
+                acc.y = -acc.y;
+            }
+            // else
+            // {
+            //     acc.y -= g;
+            //     Debug.Log("dupa");
+            // }
+
+            acc.x = -acc.x;
+
+            unchangedAcc = acc;
+
+
+            if (Mathf.Abs(acc.x) <= minimalAccelerationThreshold.x ||
+                Mathf.Abs(acc.x) <= activateDirectionThreshold.x)
+            {
+                acc.x = 0;
+            }
+
+            if (Mathf.Abs(acc.y) <= minimalAccelerationThreshold.y ||
+                Mathf.Abs(acc.y) <= activateDirectionThreshold.y)
+            {
+                acc.y = 0;
+            }
+
+            if (Mathf.Abs(acc.x) * unintentionalDirectionActivationGapMultiplier.x < Mathf.Abs(acc.y))
+            {
+                acc.x = 0;
+            }
+
+            if (Mathf.Abs(acc.y) * unintentionalDirectionActivationGapMultiplier.y < Mathf.Abs(acc.x))
+            {
+                acc.y = 0;
+            }
+
+            if (Mathf.Abs(gyroRate.x) > gyroDelta.y)
+            {
+                acc.y = 0;
+            }
+
+            if (Mathf.Abs(gyroRate.y) > gyroDelta.x)
+            {
+                acc.x = 0;
+            }
         }
         else
         {
-            acc.y -= g;
-            acc.y = -acc.y;
-        }
-        // else
-        // {
-        //     acc.y -= g;
-        //     Debug.Log("dupa");
-        // }
-        
-        acc.x = -acc.x;
-        
-        unchangedAcc = acc;
-        
-        
-        if (Mathf.Abs(acc.x) <= minimalAccelerationThreshold.x ||
-            Mathf.Abs(acc.x) <= activateDirectionThreshold.x)
-        {
-            acc.x = 0;
-        }
-        
-        if (Mathf.Abs(acc.y) <= minimalAccelerationThreshold.y ||
-            Mathf.Abs(acc.y) <= activateDirectionThreshold.y)
-        {
-            acc.y = 0;
-        }
-        
-        if (Mathf.Abs(acc.x) * unintentionalDirectionActivationGapMultiplier.x < Mathf.Abs(acc.y))
-        {
-            acc.x = 0;
-        }
-        
-        if (Mathf.Abs(acc.y) * unintentionalDirectionActivationGapMultiplier.y < Mathf.Abs(acc.x))
-        {
-            acc.y = 0;
+            acc.y = -gyroRate.x;
+            acc.x = gyroRate.y;
+
+            if (Mathf.Abs(acc.x) <= activateDirectionThreshold.x)
+            {
+                acc.x = 0;
+            }
+
+            if (Mathf.Abs(acc.y) <= activateDirectionThreshold.y)
+            {
+                acc.y = 0;
+            }
+            
+            Debug.Log(acc);
         }
 
-        if (Mathf.Abs(gyroRate.x) > gyroDelta.y)
-        {
-            acc.y = 0;
-        }
-
-        if (Mathf.Abs(gyroRate.y) > gyroDelta.x)
-        {
-            acc.x = 0;
-        }
         
-        Debug.Log(gyroRate.y);
-
         if (!Active && acc.magnitude >= activateAccelerometrThreshold)
         {
             StartCoroutine(ActivateAccelerometr(acc.normalized));
