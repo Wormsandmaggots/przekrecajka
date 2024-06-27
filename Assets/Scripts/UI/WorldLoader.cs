a5144e01f8c8e476c47e963c49d090217a4fffd2
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2024-06-27 06:55:18 +0200

    .

--- Assets/Scripts/UI/WorldLoader.cs
+++ Assets/Scripts/UI/WorldLoader.cs
@@ -7,9 +7,9 @@ using UnityEngine.UI;
 
 public class WorldLoader : MonoBehaviour
 {
-    private static string path = @"save.txt";
-    //true - unlocked
-    //false - locked
+    
+    //true - locked
+    //false - unlocked
     public static Dictionary<string, Dictionary<string, bool>> data = new Dictionary<string, Dictionary<string, bool>>
         { };
     private static string saveString;
@@ -19,12 +19,8 @@ public class WorldLoader : MonoBehaviour
     {
         if (data.Count <= 0)
         {
-            if (!File.Exists(path))
+            if (true)
             {
-                using (FileStream fs = File.Create(path))
-                {
-                }
-
                 bool first = true;
 
                 pickers = FindObjectsByType<WorldPicker>(FindObjectsSortMode.InstanceID);
@@ -38,12 +34,14 @@ public class WorldLoader : MonoBehaviour
                     {
                         saveString += "w1";
                         world.Locked = false;
+                        data[world.worldName].Add("w", false);
                     }
                     else
                     {
                         saveString += "w0";
                         world.Locked = true;
                         world.GetComponent<Image>().DOFade(0.2f, 1);
+                        data[world.worldName].Add("w", true);
                     }
 
                     foreach (var level in world.levels)
@@ -51,89 +49,86 @@ public class WorldLoader : MonoBehaviour
                         if (first)
                         {
                             saveString += " " + level.text.text + "1";
-                            data[world.worldName].Add(level.text.text, true);
+                            data[world.worldName].Add(level.text.text, false);
                             level.Locked = false;
                             first = false;
                             continue;
                         }
 
                         saveString += " " + level.text.text + "0";
-                        data[world.worldName].Add(level.text.text, false);
+                        data[world.worldName].Add(level.text.text, true);
                         level.GetComponent<Image>().DOFade(0.5f, 1);
                     }
 
                     saveString += "\n";
                 }
-
-                File.WriteAllText(path, saveString);
-
             }
             else
             {
 
-                using (StreamReader reader = new StreamReader(path))
-                {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        saveString += line + "\n";
-                    }
-                }
+                // using (StreamReader reader = new StreamReader(path))
+                // {
+                //     string line;
+                //     while ((line = reader.ReadLine()) != null)
+                //     {
+                //         saveString += line + "\n";
+                //     }
+                // }
 
-                string[] s1 = saveString.Split("\n");
-                string[] s = new string[s1.Length - 1];
-
-                for (int j = 0; j < s.Length; j++)
-                {
-                    s[j] = s1[j];
-                }
-
-                string[][] ss = new string [s.Length][];
-
-                int i = 0;
-                foreach (var line in s)
-                {
-                    ss[i] = s[i].Split(" ");
-                    i++;
-                }
-
-                foreach (var line in ss)
-                {
-                    for (int j = 0; j < line.Length; j++)
-                    {
-                        if (j == 0)
-                        {
-                            data.Add(line[j], new Dictionary<string, bool>());
-                            continue;
-                        }
-
-                        data[line[0]].Add(line[j][0].ToString(), line[j][1] == '0');
-                    }
-                }
-
-                pickers = FindObjectsByType<WorldPicker>(FindObjectsSortMode.InstanceID);
-
-                int k = 0;
-                foreach (var picker in pickers)
-                {
-                    picker.Locked = data[picker.worldName]["w"];
-                    
-                    if (picker.Locked)
-                    {
-                        picker.GetComponent<Image>().DOFade(0.2f, 1);
-                    }
-
-
-                    for (int j = 0; j < picker.levels.Length; j++)
-                    {
-                        picker.levels[j].Locked = data[picker.worldName][picker.levels[j].text.text];
-                        
-                        if (picker.levels[j].Locked)
-                        {
-                            picker.levels[j].GetComponent<Image>().DOFade(0.5f, 1);
-                        }
-                    }
-                }
+                // string[] s1 = saveString.Split("\n");
+                // string[] s = new string[s1.Length - 1];
+                //
+                // for (int j = 0; j < s.Length; j++)
+                // {
+                //     s[j] = s1[j];
+                // }
+                //
+                // string[][] ss = new string [s.Length][];
+                //
+                // int i = 0;
+                // foreach (var line in s)
+                // {
+                //     ss[i] = s[i].Split(" ");
+                //     i++;
+                // }
+                //
+                // foreach (var line in ss)
+                // {
+                //     for (int j = 0; j < line.Length; j++)
+                //     {
+                //         if (j == 0)
+                //         {
+                //             data.Add(line[j], new Dictionary<string, bool>());
+                //             continue;
+                //         }
+                //
+                //         data[line[0]].Add(line[j][0].ToString(), line[j][1] == '0');
+                //     }
+                // }
+                //
+                // pickers = FindObjectsByType<WorldPicker>(FindObjectsSortMode.InstanceID);
+                //
+                // int k = 0;
+                // foreach (var picker in pickers)
+                // {
+                //     picker.Locked = data[picker.worldName]["w"];
+                //     
+                //     if (picker.Locked)
+                //     {
+                //         picker.GetComponent<Image>().DOFade(0.2f, 1);
+                //     }
+                //
+                //
+                //     for (int j = 0; j < picker.levels.Length; j++)
+                //     {
+                //         picker.levels[j].Locked = data[picker.worldName][picker.levels[j].text.text];
+                //         
+                //         if (picker.levels[j].Locked)
+                //         {
+                //             picker.levels[j].GetComponent<Image>().DOFade(0.5f, 1);
+                //         }
+                //     }
+                // }
             }
         }
         else
@@ -180,7 +175,7 @@ public class WorldLoader : MonoBehaviour
             saveString += "\n";
         }
             
-        File.WriteAllText(path, saveString);
+        //File.WriteAllText(path, saveString);
     }
 
     public static void UpdateSave(string worldName, string level, bool value)
